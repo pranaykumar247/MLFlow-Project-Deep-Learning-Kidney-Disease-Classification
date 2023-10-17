1eda613f73f3b469d5292357e1caf20917a25f06
Pranay Kumar <pranaykumar074@gmail.com>
2023-10-17 23:02:55 +0530

    Data Ingestion Added

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-## Demo File
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-from src.cnnClassifier import logger
+STAGE_NAME = "Data Ingestion stage"
 
-logger.info("Welcome to our custom log")
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,5 @@
+## This file is used as it will not change for any instance
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
